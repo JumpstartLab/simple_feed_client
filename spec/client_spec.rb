9b166ca8f6be5f6fa197ebff87929917ddcea474
Jeff Casimir <jeff@casimircreative.com>
2012-05-21 10:21:14 -0400

    Setup with failing test for configurability

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -1,8 +1,19 @@
 Bundler.require
 
 describe SimpleFeed::Client do 
-  it "can get feeds" do
-    SimpleFeed::Client.new.should respond_to(:get_feeds)
+  let(:client) do
+    SimpleFeed::Client.new(:basic_auth => {:login => 'j3', :password => 'hungry'})
   end
+
+  describe ".get_feeds" do
+    it "can get feeds" do
+      client.should respond_to(:get_feeds)
+    end
+
+    it "gets a 200 response" do
+      client.get_feeds.first.status.should == 200
+    end
+  end
+
   
 end
